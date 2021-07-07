4fed4b57206d9bdd9e6ad6ee98d5e94529bfb6d8
nasrullahmansur <nasrullah.cit.bd@gmail.com>
2021-07-08 01:10:08 +0600

    some page created

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
     "use strict";
 
-    $('#location-search').on('click', function() {
-        $(this).next('.search-mega').toggle();
-    })
+    $("#location-search").on("click", function () {
+        $(this).next(".search-mega").toggle();
+    });
 
+    // Share;
+    $(".share > i").on("click", function () {
+        $(".share .share-links").toggleClass("active");
+    });
 
-// slide-item-one;
-if($('.slide-item-one').length > 0) {
-    $('.slide-item-one').slick({
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
-      });
+    // slide-item-one;
+    if ($(".slide-item-one").length > 0) {
+        $(".slide-item-one").slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        });
     }
 
+    if ($(".products-slide").length > 0) {
+        $(".products-slide").slick({
+            infinite: true,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+        });
+    }
 
-if($('.slider-item-two').length > 0) {
-    $('.slider-item-two').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-      });
+    if ($(".is-sticky").length > 0) {
+        $(".is-sticky").sticksy({
+            topSpacing: 30,
+        });
+    }
+
+    if ($("#agree").length > 0) {
+        $("#agree").on("change", function () {
+            $("#mail-preferences").slideToggle();
+        });
+    }
+
+    if ($(".slider-item-two").length > 0) {
+        $(".slider-item-two").slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+        });
     }
 });
